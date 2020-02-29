76dc7e321176ecf0b97f17f8f06e02ded1f8e634
Alberto Vega <albertovega00@gmail.com>
2020-02-28 17:15:45 -0800

    added styling sheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Header from "./Components/Header"
+import "./style.css"
 
 export default class App extends Component {
  
     render() {
       return(
-          <div>
-            <Header/>
+        <div>
+        <Header/>
           </div>
       )
     }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ export default class Header extends Component {
 
 render() {
     return(
-        <div>
+        <div className="header">
             <h3>Hello World</h3>
         </div>
     )
