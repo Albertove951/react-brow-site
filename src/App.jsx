91cc7a81161eb37c52b4c920510888b8220cd8b7
Alberto Vega <albertovega00@gmail.com>
2020-03-02 12:14:59 -0800

    added styling to the header

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ export default class App extends Component {
       return(
         <div>
         <Header/>
-          </div>
+        </div>
       )
     }
-}
+    
+  }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,8 +4,8 @@ export default class Header extends Component {
 
 render() {
     return(
-        <div className="header">
-            <h3>Hello World</h3>
+        <div className="header ">
+            <h3 className="text-align-center header-font">My Website</h3>
         </div>
     )
 }
