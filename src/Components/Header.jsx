dde9e18b84ebaa2cce1bfa5c081e04b23c1ed85b
Alberto Vega <albertovega00@gmail.com>
2020-03-05 14:50:23 -0800

    added functionality to the buttons to scroll down to component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,35 @@
 import React, { Component } from "react";
 
 export default class Header extends Component {
+    constructor(props){
+        super(props);
+    
+        this.onScrollAboutMe = this.onScrollAboutMe.bind(this);
+        this.onScrollService = this.onScrollService.bind(this);
+        this.onScrollTestimonial = this.onScrollTestimonial.bind(this);
+        this.onScrollCalendar = this.onScrollCalendar.bind(this);
+    }
+
+    // change to all run on one function
+    onScrollAboutMe() {
+        let element = document.getElementById("about-me");
+        element.scrollIntoView({behavior: "smooth"});
+    }
+
+    onScrollService() {
+        let element = document.getElementById("service");
+        element.scrollIntoView({behavior: "smooth"});
+    }
+
+    onScrollTestimonial() {
+        let element = document.getElementById("testimonial");
+        element.scrollIntoView({behavior: "smooth"});
+    }
+
+    onScrollCalendar() {
+        let element = document.getElementById("calendar");
+        element.scrollIntoView({behavior: "smooth"});
+    }
 
     render() {
         return (
@@ -8,18 +37,18 @@ export default class Header extends Component {
                 <h3 className="header-title ">My Website.</h3>
                 <div className="container">
                     <div className="header-row">
+                        {/* <div className="vertical-line"></div>
+                        <p className="contact">Contact Me</p> */}
                         <div className="vertical-line"></div>
-                        <p className="contact">Contact Me</p>
+                        <p className="contact" onClick={this.onScrollAboutMe}>About me</p>
                         <div className="vertical-line"></div>
-                        <p className="contact">Location</p>
+                        <p className="contact" onClick={this.onScrollService}>Services</p>
                         <div className="vertical-line"></div>
-                        <p className="contact">Appointments</p>
+                        <p className="contact" onClick={this.onScrollTestimonial}>Testimonials</p>
+                        {/* <div className="vertical-line"></div>
+                        <p className="contact">Location</p> */}
                         <div className="vertical-line"></div>
-                        <p className="contact">Services</p>
-                        <div className="vertical-line"></div>
-                        <p className="contact">About me</p>
-                        <div className="vertical-line"></div>
-                        <p className="contact">Testimonials</p>
+                        <p className="contact" onClick={this.onScrollCalendar}>Appointments</p>
                     </div>
                 </div>
             </div>
