034d1bd0ba67a3bc9f7df8f7ec3f2c65eccbff78
Alberto Vega <albertovega00@gmail.com>
2020-03-05 12:19:07 -0800

    changed image to wall of text

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,16 @@ export default class App extends Component {
       return(
         <div className="background">
         <Header/>
-        <div className="void">
+        {/* <div className="void"> */}
         <h3 className="font">something to fill the void</h3>
-        </div>
+        <p className="void">
+          Pharetra pharetra massa massa ultricies mi. Felis imperdiet proin fermentum leo vel orci porta non. Lacus
+           vestibulum sed arcu non odio. Fusce ut placerat orci nulla. In ornare quam viverra orci sagittis eu 
+           volutpat odio. Consequat nisl vel pretium lectus quam id leo. Enim nec dui nunc mattis enim ut tellus. 
+           Lacus sed turpis tincidunt id aliquet risus feugiat in. Eu tincidunt tortor aliquam nulla. Rhoncus urna 
+           neque viverra justo nec ultrices dui. 
+           </p>
+        {/* </div> */}
         <AboutMe/>
         <Service/>
         <Testimonial/>
