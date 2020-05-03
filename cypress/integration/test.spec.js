378024e387f7272f86fb4c45ebf11bf7466036f1
Alberto Vega <albertovega00@gmail.com>
2020-05-03 11:03:20 -0700

    added click test on about me

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,5 +1,6 @@
 describe('visit website', () => {
     it('visits the website', () => {
         cy.visit('https://polar-bastion-62065.herokuapp.com/')
+        cy.get('.header-row > :nth-child(2)').click()
     })
 })
